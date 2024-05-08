104e4696123641b161b2fc7c1b856096954f434d
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2024-05-09 01:02:13 +0900

    Fix wrong discount price

--- world_1_fundamentals/class_3_arithmetic_operators/boss_fight/challenge_12a.py
+++ world_1_fundamentals/class_3_arithmetic_operators/boss_fight/challenge_12a.py
@@ -4,6 +4,6 @@ Develop an algorithm that reads the price of a product and displays its price wi
 '''
 
 productPrice = float(input("Please enter the product price: "))
-discountPrice = productPrice * 0.5
+discountPrice = productPrice * 0.05
 finalPrice = productPrice - discountPrice
 print(f"The price of the product with a 5% discount is ${finalPrice}")
